6ddfd31df87c02904f89478126b236e3c2cf099a
Maria <maria.mandrykina@gmail.com>
2023-06-21 15:20:08 +0300

    add test class Queue

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,39 @@
 """Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+from src.queue import Queue
+
+
+class TestStack(unittest.TestCase):
+
+    def test_init(self):
+        assert str(Queue()) == ""
+
+
+    def test_enqueue(self):
+        queue = Queue()
+        queue.enqueue('data1')
+        queue.enqueue('data2')
+        queue.enqueue('data3')
+        self.assertEqual(queue.head.data, "data1")
+        self.assertEqual(queue.head.next_node.data, "data2")
+        self.assertEqual(queue.tail.data, "data3")
+        self.assertEqual(queue.tail.next_node, None)
+
+
+    def test_dequeue(self):
+        queue = Queue()
+        queue.enqueue('data1')
+        queue.dequeue()
+        self.assertEqual(queue.head, None)
+
+
+    def test_str(self):
+        queue = Queue()
+        queue.enqueue('data1')
+        queue.enqueue('data2')
+        queue.enqueue('data3')
+        self.assertEqual(queue.head.data, "data1")
+        self.assertEqual(queue.head.next_node.data, "data2")
+        self.assertEqual(queue.tail.data, "data3")
+        self.assertEqual(queue.tail.next_node, None)
+        self.assertEqual(str(queue), "data1\ndata2\ndata3")
