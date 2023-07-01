d45c797eaeffb23a76a4c41e22e532bd18ecf86f
Maria <maria.mandrykina@gmail.com>
2023-07-02 02:09:43 +0300

    commit linked_list.py

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,40 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
 
+    def __init__(self, data):
+        """
+        Конструктор класса Node
+
+        :param data: данные, которые будут храниться в узле
+        """
+        self.next_node = None
+        self.data = data
 
 class LinkedList:
     """Класс для односвязного списка"""
-
+    head = None
+    tail = None
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+
+        node = Node(data)
+        if self.head is None:
+            self.head = self.tail = node
+        else:
+            node.next_node = self.head
+            self.head = node
+
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+
+        node = Node(data)
+        if self.head is None:
+            self.head = self.tail = node
+        else:
+            self.tail.next_node = node
+            self.tail = node
+
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
