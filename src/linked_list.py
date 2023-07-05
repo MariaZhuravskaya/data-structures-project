cfefc196a1dc24bde2551ba1853e284f90b8399c
Maria <maria.mandrykina@gmail.com>
2023-07-05 09:48:26 +0300

    commit add - `to_list()` - `get_data_by_id()`

--- src/linked_list.py
+++ src/linked_list.py
@@ -10,12 +10,16 @@ class Node:
         self.next_node = None
         self.data = data
 
+
 class LinkedList:
     """Класс для односвязного списка"""
     head = None
     tail = None
+
     def insert_beginning(self, data: dict) -> None:
-        """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
+        """
+        Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка
+        """
 
         node = Node(data)
         if self.head is None:
@@ -24,9 +28,10 @@ class LinkedList:
             node.next_node = self.head
             self.head = node
 
-
     def insert_at_end(self, data: dict) -> None:
-        """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
+        """
+        Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка
+        """
 
         node = Node(data)
         if self.head is None:
@@ -35,9 +40,44 @@ class LinkedList:
             self.tail.next_node = node
             self.tail = node
 
+    def to_list(self):
+        """
+        Метод озвращает список с данными, содержащимися в односвязном списке `LinkedList`
+        """
+        node = self.head
+        if node is None:
+            return str(None)
+
+        ll_string = []
+        while node:
+            ll_string.append(node.data)
+            node = node.next_node
+        return ll_string
+
+    def get_data_by_id(self, id):
+        """
+        Метод возвращает первый найденный в `LinkedList` словарь с ключом 'id',
+        значение которого равно переданному в метод значению.
+        Блок `try/except` перехвает ошибки `TypeError` и выводит сообщение
+        'Данные не являются словарем или в словаре нет id.'
+        """
+        node = self.head
+        if node is None:
+            return str(None)
+
+        while node:
+            try:
+                node = node.next_node
+                if node.data['id'] == id:
+                    return node.data
+            except TypeError:
+                print('Данные не являются словарем или в словаре нет id.')
+
 
     def __str__(self) -> str:
-        """Вывод данных односвязного списка в строковом представлении"""
+        """
+        Вывод данных односвязного списка в строковом представлении
+        """
         node = self.head
         if node is None:
             return str(None)
